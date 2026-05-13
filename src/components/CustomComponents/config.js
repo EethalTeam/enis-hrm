dc59886acf7fc704e9f0d79a28802a1d9db18d16
eethaldevteam123 <eethalnaditsolutions@gmail.com>
2026-05-13 18:56:06 +0530

    8001

--- src/components/CustomComponents/config.js
+++ src/components/CustomComponents/config.js
@@ -1,6 +1,6 @@
 export const config = {
-  Api: "http://127.0.0.1:8001", //Local server
+  // Api: "http://127.0.0.1:8001", //Local server
   // Api:'https://enis-hrm-node.onrender.com',                                  //Snab server
   // Api:'https://eethal-hrm-node.onrender.com',                                 // Eethal server
-  // Api: "https://eethal-hrm-node.eniscloud.in", //  vps server
+  Api: "https://eethal-hrm-node.eniscloud.in", //  vps server
 };
